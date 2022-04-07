b9fba79fa75d1294133fe68bd5912e154c0b6bb2
prof-salu <saluorodrigues@gmail.com>
2022-04-07 20:17:28 -0300

    agenda-contatos-final

--- agenda-contatos/App.js
+++ agenda-contatos/App.js
@@ -12,6 +12,8 @@ export default class App extends React.Component {
   state = {
     nome : '',
     tel: '',
+    inputNome: React.createRef(),
+    inputTel: React.createRef(),
     lista: [
       { id: 0, nome: "Carlos", tel: "1234" },
       { id: 1, nome: "Tereza", tel: "5678" },
@@ -21,10 +23,51 @@ export default class App extends React.Component {
   chave = this.state.lista.length;
 
   addContato = this.addContato.bind(this);
+  apagarContato = this.apagarContato.bind(this);
+  editarContato = this.editarContato.bind(this);
+
+  resetInputs(){
+    this.state.nome = '';
+    this.state.tel = '';
+
+    this.state.inputNome.current.value = '';
+    this.state.inputTel.current.value = '';
+  }
+
+  editarContato(){
+    const indice = this.state.lista.findIndex(
+      item => (item.nome == this.state.nome));
+      
+    this.state.lista[indice].tel = this.state.tel;
+
+    this.resetInputs();
+
+    this.setState(this.state.lista);
+
+  }
+
+  apagarContato(){
+    const filtro = this.state.lista.filter(
+      item => ( item.nome != this.state.nome || 
+                item.tel != this.state.tel));
+      
+      console.log(filtro);
+
+      this.resetInputs();
+
+      this.setState({lista: filtro});
+  }
+
+
 
   addContato() {
+    if(this.state.nome == '' || this.state.tel == ''){
+      return;
+    }
     this.state.lista.push({ id: ++this.chave, nome: this.state.nome, tel: this.state.tel });
     this.setState(this.state);
+
+    this.resetInputs();
   }
 
   render() {
@@ -34,26 +77,34 @@ export default class App extends React.Component {
           <TextInput 
             style={styles.entrada} 
             placeholder="Nome" 
+            ref={this.state.inputNome}
             onChangeText={(nome) => {this.setState({nome})}}
           />
 
           <TextInput 
             style={styles.entrada} 
             placeholder="Telefone" 
+            ref={this.state.inputTel}
             onChangeText={(tel) => {this.setState({tel})}}
           />
         </View>
 
         <View>
-          <TouchableOpacity style={styles.botao} onPress={this.addContato}>
+          <TouchableOpacity 
+          style={styles.botao} 
+          onPress={this.addContato}>
             <Text style={styles.textoBotao}>Adicionar</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity 
+          style={styles.botao}
+          onPress={this.apagarContato}>
             <Text style={styles.textoBotao}>Apagar</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity 
+          style={styles.botao}
+          onPress={this.editarContato}>
             <Text style={styles.textoBotao}>Editar</Text>
           </TouchableOpacity>
         </View>
